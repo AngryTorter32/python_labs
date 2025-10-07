8989cc568934e1ed2fe4f8e7afdee1ae8fef75f2
AngryTorter32 <kuznetsovilusha@gmail.com>
2025-10-08 00:55:56 +0300

    08.10.25 (0:55)

--- scr/lab02/tuples.py
+++ scr/lab02/tuples.py
@@ -1,4 +1,20 @@
 def tuples(a):
     if type(a[0]) != str or type(a[1]) != str or type(a[2]) != float:
         return 'TypeError'
-    if #написать везде догтеги
+    if a[2] > 5.00:
+        return "ValueError"
+    name = a[0].split()
+    if len(name) < 2:
+        return 'ValueError'
+    for i in range(len(name)):
+        name[i] = name[i].capitalize()
+    fio = str(name[0]) + ' '
+    for i in range(1, len(name)):
+        fio += name[i][0]
+        fio += '. '
+    fio = fio[:-1]
+    return fio + ', ' + str(a[1])+ ', GPA ' + str(round(a[2], 2))
+print(tuples(("Иванов Иван Иванович", "BIVT-25", 4.6)))
+print(tuples(("Петров Пётр", "IKBO-12", 5.0)))
+print(tuples(("Петров Пётр Петрович", "IKBO-12", 5.0)))
+print(tuples(("  сидорова  анна   сергеевна ", "ABB-01", 3.999)))
