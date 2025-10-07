90191f995348dc46443afc07578e55f06f45de28
AngryTorter32 <kuznetsovilusha@gmail.com>
2025-10-08 01:18:25 +0300

    lil_update_2

--- scr/lab02/arrays.py
+++ scr/lab02/arrays.py
@@ -17,10 +17,10 @@ def flatten(c):
     return d
 print('min_max:')
 print(min_max([3, -1, 5, 5, 0]), min_max([42]), min_max([-5, -2, -9]), sep = '\n')
-print(min_max([]), min_max([1.5, 2, 2.0, -3.1]), sep = '\n')
+print(min_max([]), min_max([1.5, 2, 2.0, -3.1]), ' ', sep = '\n')
 print('unique_sorted:')
 print(unique_sorted([3, 1, 2, 1, 3]), unique_sorted([]), sep = '\n')
-print(unique_sorted([-1, -1, 0, 2, 2]), unique_sorted([1.0, 1, 2.5, 2.5, 0]), sep = '\n')
+print(unique_sorted([-1, -1, 0, 2, 2]), unique_sorted([1.0, 1, 2.5, 2.5, 0]), ' ', sep = '\n')
 print('flatten:')
 print(flatten([[1, 2], [3, 4]]), flatten(([1, 2], (3, 4, 5))), flatten([[1], [], [2, 3]]), sep = '\n')
 print(flatten([[1, 2], "ab"]))
