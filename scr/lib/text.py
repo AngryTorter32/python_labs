9bb1aa32807b261edc08bffdaa4c213c35049698
AngryTorter32 <kuznetsovilusha@gmail.com>
2025-10-16 15:19:16 +0300

    16.10.25 (15:19)

--- scr/lib/text.py
+++ scr/lib/text.py
@@ -16,15 +16,26 @@ def normalize(text, casefold, yo2e):
     text = text.strip()
     return text
 def tokenize(text):
+    raz = ['!', ',', '😀', '—', ':', ';', '?', '/', '&', '*', '#', '$', '%']
+    for i in range(len(text)):
+        if text[i] in raz:
+            text = text.replace(text[i], ' ')
     spis = text.split()
-    text = ''
-    for i in range(len(spis)):
-        text += spis[i]
-        text += ' '
-    text = text.strip()
     return spis
+def count_freq(text):
+    sl = set(text)
+    sl = list(sl)
+    kol = []
+    for i in range(len(sl)):
+        a = str(sl[i]) + ':' + str(text.count(sl[i]))
+        kol.append(a)
+    return kol
 print('normalize:')
 print(normalize("ПрИвЕт\nМИр\t", True, True), normalize('ёжик, Ёлка', True, True), sep = '\n')
 print(normalize("Hello\r\nWorld", True, True), normalize("  двойные   пробелы  ", True, True), sep='\n')
 print('', 'tokenize:', sep = '\n')
 print(tokenize("привет мир"), tokenize("hello,world!!!"), sep = '\n')
+print(tokenize("по-настоящему круто"), tokenize("2025 год"), sep = '\n')
+print(tokenize("emoji 😀 не слово"))
+print(' ', 'count_freq + top_n:', sep = '\n')
+print(count_freq(["a","b","a","c","b","a"]))
