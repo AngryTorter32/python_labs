ff1a1fbf98056e9428486f96ca253eca30235606
AngryTorter32 <kuznetsovilusha@gmail.com>
2025-10-21 13:14:20 +0300

    test

--- scr/lab03/text_stats.py
+++ scr/lab03/text_stats.py
@@ -8,6 +8,8 @@ for i in range(len(text)):
     ls.append(len(text[i]))
 print('Слово', ' ' * (max(ls) - len('Слово')), '| Частота', sep = '')
 print('-' * max(ls), '-', '-' * len(' частота'), sep = '')
+fin = []
 for i in range(len(set(text))):
-    print(list(set(text))[i], ' ' * (max(ls) - len(list(set(text))[i])), '| ', text.count(list(set(text))[i]), sep = '')
+    fin.append(list(set(text))[i], ' ' * (max(ls) - len(list(set(text))[i])), '| ', text.count(list(set(text))[i]), sep = '')
+    
 #запускать командой python -m scr.lab03.text_stats
