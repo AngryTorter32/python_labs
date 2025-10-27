1109e480f571a62900cf33820a2d4bc53a639099
AngryTorter32 <kuznetsovilusha@gmail.com>
2025-10-27 18:56:54 +0300

    27.10.2025 (18:56)

--- scr/lab03/text_stats.py
+++ scr/lab03/text_stats.py
@@ -4,13 +4,9 @@ text = normalize(text, True, True)
 text = tokenize(text)
 print('Всего слов:', len(text))
 print('Уникальных слов:', len(set(text)))
-ls = []
-for i in range(len(text)):
-    ls.append(len(text[i]))
-print('Слово', ' ' * (max(ls) - len('Слово')), '| Частота', sep = '')
-print('-' * max(ls), '-', '-' * len(' частота'), sep = '')
+sl = []
 top = top_n(text, 5)
-for i in range(len(top)):
-    print(text[i], ' ' * (max(ls) - len(text[i])), '| ', top.get(text[i]))
-print(text, top)
+k = top.keys()
+n = list(k)
+print(top, n)
 #запускать командой python -m scr.lab03.text_stats
