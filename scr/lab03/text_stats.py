b40cdf0d653e02a5c87c5dc45c9c7aaff225293c
AngryTorter32 <kuznetsovilusha@gmail.com>
2025-10-28 21:32:40 +0300

    28.10.25 (21:32)

--- scr/lab03/text_stats.py
+++ scr/lab03/text_stats.py
@@ -9,10 +9,16 @@ sl = []
 for i in range(len(sp)):
     sl.append(len(sp[i][0]))
 m = max(sl)
+print('табличный режим(on/off):')
+vkl = input()
 print('Топ-5:')
 if m < len('Слово'):
     m = len('Слово')
-print('Слово', ' ' * (m - len('Слово')), '|', ' частота', sep = '')
-print('-' * m, '-' * 9, sep = '')
-for i in range(len(sp)):
-    print(sp[i][0], ' ' * (m - len(sp[i][0])), '| ', sp[i][1], sep = '')
+if vkl == 'on':
+    print('Слово', ' ' * (m - len('Слово')), '|', ' частота', sep = '')
+    print('-' * m, '-' * 9, sep = '')
+    for i in range(len(sp)):
+        print(sp[i][0], ' ' * (m - len(sp[i][0])), '| ', sp[i][1], sep = '')
+else:
+    for i in range(len(sp)):
+        print(sp[i][0], ':', sp[i][1], sep = '')
