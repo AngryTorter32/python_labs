8846a03408faca0231566e6b56025ae4a747fce4
AngryTorter32 <kuznetsovilusha@gmail.com>
2025-12-10 00:12:56 +0300

    rework

--- src/lab04/test_for_readme.py
+++ src/lab04/test_for_readme.py
@@ -1,3 +1,5 @@
+"""
 from io_txt_csv import read_text, write_csv
 txt = read_text(r"C:\Users\kuzne\Desktop\laby_piton\python_labs\src\data\lab04\input.txt")  # должен вернуть строку
 write_csv([("word","count"),("test",3)], "data/check.csv")  # создаст CSV
+"""
